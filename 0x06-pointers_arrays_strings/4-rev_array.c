b2dfbe4a94f54d216d309b5d58e21c3376985f03
ThengweTheFirst <109463825+ThengweTheFirst@users.noreply.github.com>
2022-09-22 15:23:42 +0200

    fixed

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -2,18 +2,41 @@
 #include "main.h"
 
 /**
-*  reverse_array - function that reverse content of array
-*  @a: array
-*  @n: number of elements
+*  _strcmp - Function which compare two strings and
+*@s1: first string
+*@s2:second string
+*Return:
+*		returns zero if s1 == s2
+*		returns negative number if s1 < s2
+*		returns positive number if s1 > s2
 */
 
-void reverse_array(int *a, int n)
+int _strcmp(char *s1, char *s2)
 {
-	int tmp, index;
-	for (index = n - 1; index > n / 2; index--)
+	int i = 0, diff = 0;
+
+	while (1)
 	{
-	tmp = a[n - 1 - index];
-	a[n - 1 - index] = a[index];
-	a[index] = tmp;
+		if (s1[i] == '\0' && s2[i] == '\0')
+			break;
+		else if (s1[i] == '\0')
+		{
+			diff = s2[i];
+			break;
+		}
+		else if (s2[i] == '\0')
+		{
+			diff = s1[i];
+			break;
+		}
+		else if (s1[i] != s2[i])
+		{
+			diff = s1[i] - s2[i];
+			break;
+		}
+		else
+			i++;
+
 	}
+	return (diff);
 }
