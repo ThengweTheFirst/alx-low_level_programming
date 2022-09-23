ff793a404b1115f40b4458a74f3d7dcc090c51c3
ThengweTheFirst <sithebemzwandie40@gmail.com>
2022-09-23 09:45:40 +0200

    complete

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,42 +1,36 @@
 #include "main.h"
 
 /**
- * print_number - function to print int using _putchar
- *
- * @num: int to print
+ * print_number -> prints an integer
+ * @n: input character
+ * Return: nothing
  */
-void print_number(int num)
-{
-	unsigned int d = 10, n;
 
-	if (num < 0)
+void print_number(int n)
+{
+	unsigned int m, d, count;
+
+	if (n < 0)
 	{
-		_putchar('-');
-		num *= -1;
-	}
-	n = num;
-	if (n < d)
-	{
-		_putchar('0' + n);
+		_putchar(45);
+		m = n * -1;
 	}
 	else
 	{
-		while (n >= d)
-		{
-			d *= 10;
-			if (d >= 1000000000)
-			if (d == 1000000000)
-				break;
-		}
-		if (!(d >= 1000000000) || n > 100000000)
-		if (!(d == 1000000000) || n == 123456789)
-			d /= 10;
-		_putchar('0' + n / d);
-		while (d != 10)
-		{
-			d /= 10;
-			_putchar('0' + (n / d) % 10);
-		}
-		_putchar('0' + n % 10);
+		m = n;
+	}
+
+	d = m;
+	count = 1;
+
+	while (d > 9)
+	{
+		d /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
 	}
 }
