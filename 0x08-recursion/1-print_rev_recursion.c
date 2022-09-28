db3583367199d87e301458db1cb72f33a87cc581
ThengweTheFirst <sithebemzwandie40@gmail.com>
2022-09-28 19:20:54 +0200

    mandatory

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_rev_recursion - prints a string in reverse
+ * @s: the string to be printed
+ *
+ * Return: nothing
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
+
---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * pow_recursion - returns the value of @x raised to the power of @y.
+ * @x: the number used
+ * @y: the exponent used
+ *
+ * Return: @x to the power @y
+ */
+
+int _pow_recursion(int x, int  y)
+{
+	if (y < 0)
+		return (-1);
+
+	else if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
+
