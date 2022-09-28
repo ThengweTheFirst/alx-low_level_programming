4f3bce335fd018ba69f2c2dfbbb8a4ad6c01761a
ThengweTheFirst <sithebemzwandie40@gmail.com>
2022-09-28 21:17:43 +0200

    all good

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -10,10 +10,15 @@
  */
 int _pow_recursion(int x, int  y)
 {
+	int a = x;
+
 	if (y < 0)
-		return (-1);
+		return(-1);
 
 	else if (y == 0)
-		return (1);
-	return (x * _pow_recursion(x, y - 1));
+		return(1);
+
+	a *= _pow_recursion(x, y - 1);
+
+	return(a)
 }
--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,6 +2,23 @@
 #include "main.h"
 
 /**
+ * _sqrt - finds the natural square root of an inserted number
+ * @num: number used
+ * @root: root to be tested
+ * Return: If the number has a natural square root - the square root, if the number does not have a natural square root - -1.
+ */
+int sqrt(int num, int root)
+{
+	if((root * root) == num)
+		return(root);
+
+	if (root == num / 2)
+		return(-1);
+
+	return(sqrt(num, root + 1));
+}
+
+/**
  * sqrt_recursion - if @n does not have a natural square root, the function should return -1
  * @n: the number used
  *
@@ -9,23 +26,12 @@
  */
 int _sqrt_recursion(int n)
 {
-	if (n == 1 || n == 0)
-		return (n);
-	return (_sqrt(0, n));
-}
+	int root = 0;
 
-/**
- * _sqrt - returns the square root of a number
- * @n: test number
- * @s: squared number
- *
- * Return: the square root of n
- */
-int _sqrt(int n, int s)
-{
-	if (n > s / 2)
-		return (-1);
-	else if (n * n == s)
-		return (n);
-	return (_sqrt(n + 1, s));
+	if (n < 0)
+		return(-1);
+
+	if (n == 1)
+		return(1);
+	return(sqrt(n, root));
 }
